8e62f04472c9f02caaaa4e6d141e57c6f380edf8
Shakib448 <shakiba448@gmail.com>
2023-03-21 22:24:14 +0600

    WIP on native apps

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const PlaceholderImage = require("./assets/ocean.jpg");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
